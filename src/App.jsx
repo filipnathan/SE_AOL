bf249932ae56e06bd2d6c7e0dab2368c3ede5855
JackniusGoh <161729699+JackniusGoh@users.noreply.github.com>
2025-06-13 12:21:33 +0700

    Payment Page
    Payment Page

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import PaymentPage from "./PaymentPage";
+
+function App() {
+  return <PaymentPage />;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import PaymentSuccessPage from "./pages/PaymentSuccessPage";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PaymentSuccessPage />
+    <App />
   </React.StrictMode>
 );
