1ff1b548858b97dfc48f3eb5555f74d794e41f0f
JackniusGoh <161729699+JackniusGoh@users.noreply.github.com>
2025-06-13 13:30:45 +0700

    Delete src directory

--- src/components/ui/button.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Button({ children, className, ...props }) {
-  return (
-    <button className={`bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
