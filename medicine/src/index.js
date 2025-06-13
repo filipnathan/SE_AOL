6819d9bab13fe780a68787742be010a06d2605d5
JackniusGoh <161729699+JackniusGoh@users.noreply.github.com>
2025-06-13 13:35:30 +0700

    Medicine Page

---
+++ medicine/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MedicinesPage from "./pages/MedicinesPage";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MedicinesPage />
+  </React.StrictMode>
+);
